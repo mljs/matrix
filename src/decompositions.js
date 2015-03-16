e37e576fbc26f9ebb66249ec9259cdfab51f017e
Michaël Zasso <mic.besace@gmail.com>
2015-03-16 13:44:55 +0100

    add inverse and solve to Matrix prototype

--- src/decompositions.js
+++ src/decompositions.js
@@ -1665,9 +1665,17 @@ function inverse(matrix) {
     return solve(matrix, Matrix.eye(matrix.rows));
 }
 
+Matrix.prototype.inverse = function () {
+    return inverse(this);
+};
+
 function solve(leftHandSide, rightHandSide) {
     return leftHandSide.isSquare() ? new LuDecomposition(leftHandSide).solve(rightHandSide) : new QrDecomposition(leftHandSide).solve(rightHandSide);
 }
+
+Matrix.prototype.solve = function (other) {
+    return solve(this, other);
+};
 
 module.exports = {
     LuDecomposition: LuDecomposition,
