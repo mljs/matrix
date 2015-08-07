cd96b4b1b1b05334aa6b1aba045a5093058eb23d
Michaël Zasso <mic.besace@gmail.com>
2015-08-07 14:33:16 +0200

    fix(matrix): abs method should return the instance

---
+++ test/matrix/abs.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var Matrix = require('../..');
+
+describe('Matrix#abs', function () {
+
+    var matrix;
+
+    beforeEach(function () {
+        matrix = new Matrix([
+            [0, 1, 2],
+            [3, -4, -5],
+            [-6, -7, -8],
+            [4.39, -0.61, -12.7]
+        ]);
+    });
+
+    it('should convert all elements to absolute value (in-place)', function () {
+        matrix.abs();
+        matrix.should.eql([
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [4.39, 0.61, 12.7]
+        ]);
+    });
+
+    it('should return instance', function () {
+        matrix.abs().should.equal(matrix);
+    });
+
+});
