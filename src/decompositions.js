4845a439c2b78214293965df480b7fdb64c38761
Michaël Zasso <mic.besace@gmail.com>
2015-09-09 08:52:54 +0200

    feat: add fullname synonyms for some methods

--- src/decompositions.js
+++ src/decompositions.js
@@ -9,17 +9,22 @@ const QrDecomposition = require('./dc/qr');
 const CholeskyDecomposition = require('./dc/cholesky');
 
 function inverse(matrix) {
+    matrix = Matrix.checkMatrix(matrix);
     return solve(matrix, Matrix.eye(matrix.rows));
 }
 
-Matrix.prototype.inverse = function () {
+Matrix.inverse = Matrix.inv = inverse;
+Matrix.prototype.inverse = Matrix.prototype.inv = function () {
     return inverse(this);
 };
 
 function solve(leftHandSide, rightHandSide) {
+    leftHandSide = Matrix.checkMatrix(leftHandSide);
+    rightHandSide = Matrix.checkMatrix(rightHandSide);
     return leftHandSide.isSquare() ? new LuDecomposition(leftHandSide).solve(rightHandSide) : new QrDecomposition(leftHandSide).solve(rightHandSide);
 }
 
+Matrix.solve = solve;
 Matrix.prototype.solve = function (other) {
     return solve(this, other);
 };
