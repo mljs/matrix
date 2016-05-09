0323a7ab7c1d4b06aad5c532ffa9dcda746e6510
Michaël Zasso <targos@protonmail.com>
2016-05-09 15:23:04 +0200

    fix benchmarks

--- benchmark/array.js
+++ benchmark/array.js
@@ -29,6 +29,6 @@ suite
         console.log(String(event.target));
     })
     .on('complete', function() {
-        console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
     })
     .run();
