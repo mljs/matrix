102ae3c99f9c2f921757f4df3adbcb799a4994b4
Michaël Zasso <mic.besace@gmail.com>
2016-05-18 19:56:12 +0200

    fix test

--- test/decompositions/evd.js
+++ test/decompositions/evd.js
@@ -7,7 +7,7 @@ describe('Eigenvalue decomposition', function () {
     it('simple example', function () {
         var matrix = new Matrix([[1, 0], [1, 3]]);
         var evd = new EVD(matrix);
-        evd.realEigenvalues.should.eql(1, 3);
+        evd.realEigenvalues.should.eql([1, 3]);
         evd.diagonalMatrix.to2DArray().should.eql([[1, 0], [0, 3]]);
     });
 });
