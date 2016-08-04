bb2f02342725f88f019fc8dee1de0eb15c6552ec
Michaël Zasso <targos@protonmail.com>
2016-08-04 08:35:32 +0200

    refactor(view tests): one less describe level

--- test/views/row.js
+++ test/views/row.js
@@ -2,31 +2,29 @@
 
 var Matrix = require('../..');
 
-describe('Matrix views', function () {
-    describe('Row view', function () {
-        it('should set and get column values', function () {
-            var m = Matrix.ones(5, 8);
-            var mrv1 = m.rowView(0);
-            var mrv2 = m.rowView(3);
+describe('Row view', function () {
+    it('should set and get column values', function () {
+        var m = Matrix.ones(5, 8);
+        var mrv1 = m.rowView(0);
+        var mrv2 = m.rowView(3);
 
-            m.set(0, 3, 5);
-            m.set(2, 2, 12);
-            m.set(3, 4, 10);
+        m.set(0, 3, 5);
+        m.set(2, 2, 12);
+        m.set(3, 4, 10);
 
-            mrv1.get(0, 3).should.equal(5);
-            mrv2.get(0, 3).should.equal(1);
+        mrv1.get(0, 3).should.equal(5);
+        mrv2.get(0, 3).should.equal(1);
 
-            mrv1.get(2, 2).should.equal(1);
-            mrv2.get(2, 2).should.equal(1);
+        mrv1.get(2, 2).should.equal(1);
+        mrv2.get(2, 2).should.equal(1);
 
-            mrv1.get(3, 4).should.equal(1);
-            mrv2.get(3, 4).should.equal(10);
+        mrv1.get(3, 4).should.equal(1);
+        mrv2.get(3, 4).should.equal(10);
 
-            mrv1.to1DArray().should.eql([1, 1, 1, 5, 1, 1, 1, 1]);
-            mrv2.to1DArray().should.eql([1, 1, 1, 1, 10, 1, 1, 1]);
+        mrv1.to1DArray().should.eql([1, 1, 1, 5, 1, 1, 1, 1]);
+        mrv2.to1DArray().should.eql([1, 1, 1, 1, 10, 1, 1, 1]);
 
-            mrv1.isRowVector().should.be.true();
-            mrv2.isRowVector().should.be.true();
-        });
+        mrv1.isRowVector().should.be.true();
+        mrv2.isRowVector().should.be.true();
     });
 });
--- test/views/transpose.js
+++ test/views/transpose.js
@@ -2,22 +2,20 @@
 
 var Matrix = require('../..');
 
-describe('Matrix views', function () {
-    describe('Transpose view', function () {
-        it('should set and get opposite coordinates', function () {
-            var m = Matrix.ones(5, 8);
-            var mtv = m.transposeView();
+describe('Transpose view', function () {
+    it('should set and get opposite coordinates', function () {
+        var m = Matrix.ones(5, 8);
+        var mtv = m.transposeView();
 
-            m.get(1, 0).should.equal(1);
-            mtv.set(0, 1, 5);
-            m.get(1, 0).should.equal(5);
+        m.get(1, 0).should.equal(1);
+        mtv.set(0, 1, 5);
+        m.get(1, 0).should.equal(5);
 
-            m.set(0, 0, 6);
-            mtv.get(0, 0).should.equal(6);
+        m.set(0, 0, 6);
+        mtv.get(0, 0).should.equal(6);
 
-            m.set(2, 1, 10);
-            mtv.get(2, 1).should.equal(1);
-            mtv.get(1, 2).should.equal(10);
-        });
+        m.set(2, 1, 10);
+        mtv.get(2, 1).should.equal(1);
+        mtv.get(1, 2).should.equal(10);
     });
 });
