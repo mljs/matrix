eaee5dec0ded6cd504d1bd086a1e51d8d9bb42f6
Michaël Zasso <targos@protonmail.com>
2016-08-04 14:18:42 +0200

    feat: make use of Symbol.species to allow creating new matrices in any class
    - defaults to the current constructor
    - views override it to return a new Matrix

--- src/views/base.js
+++ src/views/base.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var abstractMatrix = require('../abstractMatrix');
+var Matrix;
 
 class BaseView extends abstractMatrix() {
     constructor(matrix, rows, columns) {
@@ -8,6 +9,13 @@ class BaseView extends abstractMatrix() {
         this.matrix = matrix;
         this.rows = rows;
         this.columns = columns;
+    }
+
+    static get [Symbol.species]() {
+        if (!Matrix) {
+            Matrix = require('../matrix');
+        }
+        return Matrix;
     }
 }
 
