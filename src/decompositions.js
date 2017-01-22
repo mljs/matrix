f54e493bbf6f5b4b7c931b2a5f69e7499fd9fc0f
Michaël Zasso <targos@protonmail.com>
2017-01-22 14:06:43 +0100

    don't include inverse and solve in matrix prototype
    This removes the hard dependency on decompositions for the Matrix
    prototype.

--- src/decompositions.js
+++ src/decompositions.js
@@ -8,35 +8,8 @@ export function inverse(matrix) {
     return solve(matrix, Matrix.eye(matrix.rows));
 }
 
-/**
- * Returns the inverse
- * @memberOf Matrix
- * @static
- * @param {Matrix} matrix
- * @return {Matrix} matrix
- * @alias inv
- */
-Matrix.inverse = Matrix.inv = inverse;
-
-/**
- * Returns the inverse
- * @memberOf Matrix
- * @static
- * @param {Matrix} matrix
- * @return {Matrix} matrix
- * @alias inv
- */
-Matrix.prototype.inverse = Matrix.prototype.inv = function () {
-    return inverse(this);
-};
-
 export function solve(leftHandSide, rightHandSide) {
     leftHandSide = Matrix.checkMatrix(leftHandSide);
     rightHandSide = Matrix.checkMatrix(rightHandSide);
     return leftHandSide.isSquare() ? new LuDecomposition(leftHandSide).solve(rightHandSide) : new QrDecomposition(leftHandSide).solve(rightHandSide);
 }
-
-Matrix.solve = solve;
-Matrix.prototype.solve = function (other) {
-    return solve(this, other);
-};
