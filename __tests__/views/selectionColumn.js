f320dc6e70354631862f449f57c2029fbe4af6c7
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-14 10:35:31 +0200

    feat: allows to select only rows or columns as view

--- __tests__/views/selectionColumn.js
+++ __tests__/views/selectionColumn.js
@@ -1,29 +1,24 @@
 import 'should';
-
 import Matrix from '../../src';
 
-describe('Selection view', function () {
+describe('Selection column view', function () {
     it('should correctly remap coordinates', function () {
         const m = Matrix.ones(5, 8);
-        const msv = m.selectionView([1, 2], [2, 1]);
+        const msv = m.selectionColumnView([1, 2]);
 
-        m.get(1, 2).should.equal(1);
-        msv.set(0, 0, 5);
-        m.get(1, 2).should.equal(5);
-
-        m.get(2, 1).should.equal(1);
-        m.set(2, 1, 10);
-        msv.get(1, 1).should.equal(10);
+        m.get(0, 2).should.equal(1);
+        msv.set(0, 1, 5);
+        m.get(0, 2).should.equal(5);
     });
 
     it('should throw when wrong arguments or range', function () {
         const m = Matrix.ones(2, 2);
         (function () {
-            m.selectionView([1, 1, 2], [0, 2]);
+            m.selectionColumnView([1, 1, 2]);
         }).should.throw(RangeError);
 
         (function () {
-            m.selectionView([1, 1]);
+            m.selectionColumnView(1);
         }).should.throw(TypeError);
     });
 });
--- src/abstractMatrix.js
+++ src/abstractMatrix.js
@@ -8,6 +8,8 @@ import MatrixTransposeView from './views/transpose';
 import MatrixRowView from './views/row';
 import MatrixSubView from './views/sub';
 import MatrixSelectionView from './views/selection';
+import MatrixSelectionRowView from './views/selectionRow';
+import MatrixSelectionColumnView from './views/selectionColumn';
 import MatrixColumnView from './views/column';
 import MatrixFlipRowView from './views/flipRow';
 import MatrixFlipColumnView from './views/flipColumn';
@@ -1504,6 +1506,30 @@ export default function AbstractMatrix(superCtor) {
          */
         selectionView(rowIndices, columnIndices) {
             return new MatrixSelectionView(this, rowIndices, columnIndices);
+        }
+
+        /**
+         * Returns a view of the row indices
+         * @example
+         * // resulting vector is [[1,2,3], [1,2,3]]
+         * var matrix = new Matrix([[1,2,3], [4,5,6]]).selectionRowView([0, 0])
+         * @param {Array<number>} rowIndices
+         * @return {MatrixSelectionRowView}
+         */
+        selectionRowView(rowIndices) {
+            return new MatrixSelectionRowView(this, rowIndices);
+        }
+
+        /**
+         * Returns a view of the column indices
+         * @example
+         * // resulting vector is [[2, 2], [5, 5]]
+         * var matrix = new Matrix([[1,2,3], [4,5,6]]).selectionColumnView([1, 1])
+         * @param {Array<number>} columnIndices
+         * @return {MatrixSelectionColumnView}
+         */
+        selectionColumnView(columnIndices) {
+            return new MatrixSelectionColumnView(this, columnIndices);
         }
 
 
