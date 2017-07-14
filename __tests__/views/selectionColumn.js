d5c4f1fb2b83b050b5b2ee1c62aa8e00273760bd
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-14 11:21:48 +0200

    test: remove should

--- __tests__/views/selectionColumn.js
+++ __tests__/views/selectionColumn.js
@@ -1,24 +1,18 @@
-import 'should';
 import Matrix from '../../src';
 
-describe('Selection column view', function () {
-    it('should correctly remap coordinates', function () {
+describe('Selection column view', () => {
+    it('should correctly remap coordinates', () => {
         const m = Matrix.ones(5, 8);
         const msv = m.selectionColumnView([1, 2]);
 
-        m.get(0, 2).should.equal(1);
+        expect(m.get(0, 2)).toBe(1);
         msv.set(0, 1, 5);
-        m.get(0, 2).should.equal(5);
+        expect(m.get(0, 2)).toBe(5);
     });
 
-    it('should throw when wrong arguments or range', function () {
+    it('should throw when wrong arguments or range', () => {
         const m = Matrix.ones(2, 2);
-        (function () {
-            m.selectionColumnView([1, 1, 2]);
-        }).should.throw(RangeError);
-
-        (function () {
-            m.selectionColumnView(1);
-        }).should.throw(TypeError);
+        expect(() => m.selectionRowView([1, 1, 2])).toThrow(RangeError);
+        expect(() => m.selectionRowView(1)).toThrow(TypeError);
     });
 });
--- __tests__/views/selectionRow.js
+++ __tests__/views/selectionRow.js
@@ -1,24 +1,18 @@
-import 'should';
 import Matrix from '../../src';
 
-describe('Selection view', function () {
-    it('should correctly remap coordinates', function () {
+describe('Selection view', () => {
+    it('should correctly remap coordinates', () => {
         const m = Matrix.ones(5, 8);
         const msv = m.selectionRowView([1, 2]);
 
-        m.get(1, 0).should.equal(1);
+        expect(m.get(1, 0)).toBe(1);
         msv.set(0, 0, 5);
-        m.get(1, 0).should.equal(5);
+        expect(m.get(1, 0)).toBe(5);
     });
 
-    it('should throw when wrong arguments or range', function () {
+    it('should throw when wrong arguments or range', () => {
         const m = Matrix.ones(2, 2);
-        (function () {
-            m.selectionRowView([1, 1, 2]);
-        }).should.throw(RangeError);
-
-        (function () {
-            m.selectionRowView(1);
-        }).should.throw(TypeError);
+        expect(() => m.selectionRowView([1, 1, 2])).toThrow(RangeError);
+        expect(() => m.selectionRowView(1)).toThrow(TypeError);
     });
 });
