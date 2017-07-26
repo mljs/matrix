33b663086969349317658071e0109c4caf0cc9c8
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-26 14:14:58 +0200

    test: use arrays instead of instance matrices

--- src/__tests__/decompositions/lu.js
+++ src/__tests__/decompositions/lu.js
@@ -19,6 +19,24 @@ describe('LU decomposition', () => {
         expect(ltm.mmul(ltm.transpose())).toEqual(matrix);
     });
 
+    it('should work with arrays', () => {
+        // http://ch.mathworks.com/help/matlab/ref/lu.html
+        let matrix = [
+            [1, -1, -1, -1, -1],
+            [-1, 2, 0, 0, 0],
+            [-1, 0, 3, 1, 1],
+            [-1, 0, 1, 4, 2],
+            [-1, 0, 1, 2, 5]
+        ];
+
+        const lu = new LU(matrix);
+
+        const ltm = lu.lowerTriangularMatrix;
+        checkTriangular(ltm);
+
+        expect(ltm.mmul(ltm.transpose())).toEqual(new Matrix(matrix));
+    });
+
     it('should throw on bad input', () => {
         expect(() => new LU([[0, 1, 2], [0, 1, 2]]).determinant).toThrow('Matrix must be square');
     });
--- src/__tests__/decompositions/qr.js
+++ src/__tests__/decompositions/qr.js
@@ -16,6 +16,27 @@ describe('Qr decomposition', () => {
         expect(qr.isFullRank()).toBe(true);
     });
 
+    it('should work with arrays', () => {
+        // http://ch.mathworks.com/help/matlab/ref/qr.html
+        let matrix = [
+            [3 / 2, 1, 0, 0],
+            [1, 1 / 2, 1, 0],
+            [0, 1, 1 / 2, 1],
+            [0, 0, 1, 3 / 2]
+        ];
+
+        const qr = new QR(matrix);
+        expect(qr.isFullRank()).toBe(true);
+
+        let b = Matrix.columnVector([20001, 20003, 20005, 20007]);
+        expect(qr.solve(b)).toBeDeepCloseTo([
+            [8000.1714],
+            [8000.7429],
+            [8002.45714],
+            [8003.02857]
+        ], 4);
+    });
+
     it('should solve the value', () => {
         let A = new Matrix([
             [10000, 10001],
