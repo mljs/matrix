1ab3f81dccdd46ad60d99492e2de90f0856041b5
Michaël Zasso <targos@protonmail.com>
2018-05-11 23:21:11 +0200

    refactor usage of variables

--- src/dc/svd.js
+++ src/dc/svd.js
@@ -4,7 +4,7 @@ import { hypotenuse, getFilled2DArray } from './util';
 
 /**
  * @class SingularValueDecomposition
- * @link https://github.com/lutzroeder/Mapack/blob/master/Source/SingularValueDecomposition.cs
+ * @see https://github.com/accord-net/framework/blob/development/Sources/Accord.Math/Decompositions/SingularValueDecomposition.cs
  * @param {Matrix} value
  * @param {object} [options]
  * @param {boolean} [options.computeLeftSingularVectors=true]
@@ -17,7 +17,6 @@ export default class SingularValueDecomposition {
 
     var m = value.rows;
     var n = value.columns;
-    var nu = Math.min(m, n);
 
     const {
       computeLeftSingularVectors = true,
@@ -50,48 +49,33 @@ export default class SingularValueDecomposition {
       a = value.clone();
     }
 
-    var s = new Array(Math.min(m + 1, n));
+    var nu = Math.min(m, n);
+    var ni = Math.min(m + 1, n);
+    var s = new Array(ni);
     var U = getFilled2DArray(m, nu, 0);
     var V = getFilled2DArray(n, n, 0);
+
     var e = new Array(n);
     var work = new Array(m);
 
+    var si = new Array(ni);
+    for (let i = 0; i < ni; i++) si[i] = i;
+
     var nct = Math.min(m - 1, n);
     var nrt = Math.max(0, Math.min(n - 2, m));
+    var mrc = Math.max(nct, nrt);
 
-    var i;
-    var j;
-    var k;
-    var p;
-    var t;
-    var ks;
-    var f;
-    var cs;
-    var sn;
-    var max;
-    var kase;
-    var scale;
-    var sp;
-    var spm1;
-    var epm1;
-    var sk;
-    var ek;
-    var b;
-    var c;
-    var shift;
-    var g;
-
-    for (k = 0, max = Math.max(nct, nrt); k < max; k++) {
+    for (let k = 0; k < mrc; k++) {
       if (k < nct) {
         s[k] = 0;
-        for (i = k; i < m; i++) {
+        for (let i = k; i < m; i++) {
           s[k] = hypotenuse(s[k], a[i][k]);
         }
         if (s[k] !== 0) {
           if (a[k][k] < 0) {
             s[k] = -s[k];
           }
-          for (i = k; i < m; i++) {
+          for (let i = k; i < m; i++) {
             a[i][k] /= s[k];
           }
           a[k][k] += 1;
@@ -99,14 +83,14 @@ export default class SingularValueDecomposition {
         s[k] = -s[k];
       }
 
-      for (j = k + 1; j < n; j++) {
+      for (let j = k + 1; j < n; j++) {
         if (k < nct && s[k] !== 0) {
-          t = 0;
-          for (i = k; i < m; i++) {
+          let t = 0;
+          for (let i = k; i < m; i++) {
             t += a[i][k] * a[i][j];
           }
           t = -t / a[k][k];
-          for (i = k; i < m; i++) {
+          for (let i = k; i < m; i++) {
             a[i][j] += t * a[i][k];
           }
         }
@@ -114,51 +98,51 @@ export default class SingularValueDecomposition {
       }
 
       if (wantu && k < nct) {
-        for (i = k; i < m; i++) {
+        for (let i = k; i < m; i++) {
           U[i][k] = a[i][k];
         }
       }
 
       if (k < nrt) {
         e[k] = 0;
-        for (i = k + 1; i < n; i++) {
+        for (let i = k + 1; i < n; i++) {
           e[k] = hypotenuse(e[k], e[i]);
         }
         if (e[k] !== 0) {
           if (e[k + 1] < 0) {
             e[k] = 0 - e[k];
           }
-          for (i = k + 1; i < n; i++) {
+          for (let i = k + 1; i < n; i++) {
             e[i] /= e[k];
           }
           e[k + 1] += 1;
         }
         e[k] = -e[k];
         if (k + 1 < m && e[k] !== 0) {
-          for (i = k + 1; i < m; i++) {
+          for (let i = k + 1; i < m; i++) {
             work[i] = 0;
           }
-          for (j = k + 1; j < n; j++) {
-            for (i = k + 1; i < m; i++) {
+          for (let i = k + 1; i < m; i++) {
+            for (let j = k + 1; j < n; j++) {
               work[i] += e[j] * a[i][j];
             }
           }
-          for (j = k + 1; j < n; j++) {
-            t = -e[j] / e[k + 1];
-            for (i = k + 1; i < m; i++) {
+          for (let j = k + 1; j < n; j++) {
+            let t = -e[j] / e[k + 1];
+            for (let i = k + 1; i < m; i++) {
               a[i][j] += t * work[i];
             }
           }
         }
         if (wantv) {
-          for (i = k + 1; i < n; i++) {
+          for (let i = k + 1; i < n; i++) {
             V[i][k] = e[i];
           }
         }
       }
     }
 
-    p = Math.min(n, m + 1);
+    let p = Math.min(n, m + 1);
     if (nct < n) {
       s[nct] = a[nct][nct];
     }
@@ -171,33 +155,33 @@ export default class SingularValueDecomposition {
     e[p - 1] = 0;
 
     if (wantu) {
-      for (j = nct; j < nu; j++) {
-        for (i = 0; i < m; i++) {
+      for (let j = nct; j < nu; j++) {
+        for (let i = 0; i < m; i++) {
           U[i][j] = 0;
         }
         U[j][j] = 1;
       }
-      for (k = nct - 1; k >= 0; k--) {
+      for (let k = nct - 1; k >= 0; k--) {
         if (s[k] !== 0) {
-          for (j = k + 1; j < nu; j++) {
-            t = 0;
-            for (i = k; i < m; i++) {
+          for (let j = k + 1; j < nu; j++) {
+            let t = 0;
+            for (let i = k; i < m; i++) {
               t += U[i][k] * U[i][j];
             }
             t = -t / U[k][k];
-            for (i = k; i < m; i++) {
+            for (let i = k; i < m; i++) {
               U[i][j] += t * U[i][k];
             }
           }
-          for (i = k; i < m; i++) {
+          for (let i = k; i < m; i++) {
             U[i][k] = -U[i][k];
           }
           U[k][k] = 1 + U[k][k];
-          for (i = 0; i < k - 1; i++) {
+          for (let i = 0; i < k - 1; i++) {
             U[i][k] = 0;
           }
         } else {
-          for (i = 0; i < m; i++) {
+          for (let i = 0; i < m; i++) {
             U[i][k] = 0;
           }
           U[k][k] = 1;
@@ -206,20 +190,20 @@ export default class SingularValueDecomposition {
     }
 
     if (wantv) {
-      for (k = n - 1; k >= 0; k--) {
+      for (let k = n - 1; k >= 0; k--) {
         if (k < nrt && e[k] !== 0) {
-          for (j = k + 1; j < n; j++) {
-            t = 0;
-            for (i = k + 1; i < n; i++) {
+          for (let j = k + 1; j < n; j++) {
+            let t = 0;
+            for (let i = k + 1; i < n; i++) {
               t += V[i][k] * V[i][j];
             }
             t = -t / V[k + 1][k];
-            for (i = k + 1; i < n; i++) {
+            for (let i = k + 1; i < n; i++) {
               V[i][j] += t * V[i][k];
             }
           }
         }
-        for (i = 0; i < n; i++) {
+        for (let i = 0; i < n; i++) {
           V[i][k] = 0;
         }
         V[k][k] = 1;
@@ -230,11 +214,14 @@ export default class SingularValueDecomposition {
     var iter = 0;
     var eps = Number.EPSILON;
     while (p > 0) {
+      let k, kase;
       for (k = p - 2; k >= -1; k--) {
         if (k === -1) {
           break;
         }
-        if (Math.abs(e[k]) <= eps * (Math.abs(s[k]) + Math.abs(s[k + 1]))) {
+        const alpha =
+          Number.MIN_VALUE + eps * Math.abs(s[k] + Math.abs(s[k + 1]));
+        if (Math.abs(e[k]) <= alpha || Number.isNaN(e[k])) {
           e[k] = 0;
           break;
         }
@@ -242,11 +229,12 @@ export default class SingularValueDecomposition {
       if (k === p - 2) {
         kase = 4;
       } else {
+        let ks;
         for (ks = p - 1; ks >= k; ks--) {
           if (ks === k) {
             break;
           }
-          t =
+          let t =
             (ks !== p ? Math.abs(e[ks]) : 0) +
             (ks !== k + 1 ? Math.abs(e[ks - 1]) : 0);
           if (Math.abs(s[ks]) <= eps * t) {
@@ -268,19 +256,19 @@ export default class SingularValueDecomposition {
 
       switch (kase) {
         case 1: {
-          f = e[p - 2];
+          let f = e[p - 2];
           e[p - 2] = 0;
-          for (j = p - 2; j >= k; j--) {
-            t = hypotenuse(s[j], f);
-            cs = s[j] / t;
-            sn = f / t;
+          for (let j = p - 2; j >= k; j--) {
+            let t = hypotenuse(s[j], f);
+            let cs = s[j] / t;
+            let sn = f / t;
             s[j] = t;
             if (j !== k) {
               f = -sn * e[j - 1];
               e[j - 1] = cs * e[j - 1];
             }
             if (wantv) {
-              for (i = 0; i < n; i++) {
+              for (let i = 0; i < n; i++) {
                 t = cs * V[i][j] + sn * V[i][p - 1];
                 V[i][p - 1] = -sn * V[i][j] + cs * V[i][p - 1];
                 V[i][j] = t;
@@ -290,17 +278,17 @@ export default class SingularValueDecomposition {
           break;
         }
         case 2: {
-          f = e[k - 1];
+          let f = e[k - 1];
           e[k - 1] = 0;
-          for (j = k; j < p; j++) {
-            t = hypotenuse(s[j], f);
-            cs = s[j] / t;
-            sn = f / t;
+          for (let j = k; j < p; j++) {
+            let t = hypotenuse(s[j], f);
+            let cs = s[j] / t;
+            let sn = f / t;
             s[j] = t;
             f = -sn * e[j];
             e[j] = cs * e[j];
             if (wantu) {
-              for (i = 0; i < m; i++) {
+              for (let i = 0; i < m; i++) {
                 t = cs * U[i][j] + sn * U[i][k - 1];
                 U[i][k - 1] = -sn * U[i][j] + cs * U[i][k - 1];
                 U[i][j] = t;
@@ -310,34 +298,35 @@ export default class SingularValueDecomposition {
           break;
         }
         case 3: {
-          scale = Math.max(
+          const scale = Math.max(
             Math.abs(s[p - 1]),
             Math.abs(s[p - 2]),
             Math.abs(e[p - 2]),
             Math.abs(s[k]),
             Math.abs(e[k])
           );
-          sp = s[p - 1] / scale;
-          spm1 = s[p - 2] / scale;
-          epm1 = e[p - 2] / scale;
-          sk = s[k] / scale;
-          ek = e[k] / scale;
-          b = ((spm1 + sp) * (spm1 - sp) + epm1 * epm1) / 2;
-          c = sp * epm1 * (sp * epm1);
-          shift = 0;
+          const sp = s[p - 1] / scale;
+          const spm1 = s[p - 2] / scale;
+          const epm1 = e[p - 2] / scale;
+          const sk = s[k] / scale;
+          const ek = e[k] / scale;
+          const b = ((spm1 + sp) * (spm1 - sp) + epm1 * epm1) / 2;
+          const c = sp * epm1 * (sp * epm1);
+          let shift = 0;
           if (b !== 0 || c !== 0) {
-            shift = Math.sqrt(b * b + c);
             if (b < 0) {
-              shift = -shift;
+              shift = 0 - Math.sqrt(b * b + c);
+            } else {
+              shift = Math.sqrt(b * b + c);
             }
             shift = c / (b + shift);
           }
-          f = (sk + sp) * (sk - sp) + shift;
-          g = sk * ek;
-          for (j = k; j < p - 1; j++) {
-            t = hypotenuse(f, g);
-            cs = f / t;
-            sn = g / t;
+          let f = (sk + sp) * (sk - sp) + shift;
+          let g = sk * ek;
+          for (let j = k; j < p - 1; j++) {
+            let t = hypotenuse(f, g);
+            let cs = f / t;
+            let sn = g / t;
             if (j !== k) {
               e[j - 1] = t;
             }
@@ -346,7 +335,7 @@ export default class SingularValueDecomposition {
             g = sn * s[j + 1];
             s[j + 1] = cs * s[j + 1];
             if (wantv) {
-              for (i = 0; i < n; i++) {
+              for (let i = 0; i < n; i++) {
                 t = cs * V[i][j] + sn * V[i][j + 1];
                 V[i][j + 1] = -sn * V[i][j] + cs * V[i][j + 1];
                 V[i][j] = t;
@@ -361,7 +350,7 @@ export default class SingularValueDecomposition {
             g = sn * e[j + 1];
             e[j + 1] = cs * e[j + 1];
             if (wantu && j < m - 1) {
-              for (i = 0; i < m; i++) {
+              for (let i = 0; i < m; i++) {
                 t = cs * U[i][j] + sn * U[i][j + 1];
                 U[i][j + 1] = -sn * U[i][j] + cs * U[i][j + 1];
                 U[i][j] = t;
@@ -376,7 +365,7 @@ export default class SingularValueDecomposition {
           if (s[k] <= 0) {
             s[k] = s[k] < 0 ? -s[k] : 0;
             if (wantv) {
-              for (i = 0; i <= pp; i++) {
+              for (let i = 0; i <= pp; i++) {
                 V[i][k] = -V[i][k];
               }
             }
@@ -385,18 +374,18 @@ export default class SingularValueDecomposition {
             if (s[k] >= s[k + 1]) {
               break;
             }
-            t = s[k];
+            let t = s[k];
             s[k] = s[k + 1];
             s[k + 1] = t;
             if (wantv && k < n - 1) {
-              for (i = 0; i < n; i++) {
+              for (let i = 0; i < n; i++) {
                 t = V[i][k + 1];
                 V[i][k + 1] = V[i][k];
                 V[i][k] = t;
               }
             }
             if (wantu && k < m - 1) {
-              for (i = 0; i < m; i++) {
+              for (let i = 0; i < m; i++) {
                 t = U[i][k + 1];
                 U[i][k + 1] = U[i][k];
                 U[i][k] = t;
@@ -438,9 +427,8 @@ export default class SingularValueDecomposition {
     var e = this.threshold;
     var scols = this.s.length;
     var Ls = Matrix.zeros(scols, scols);
-    var i;
 
-    for (i = 0; i < scols; i++) {
+    for (let i = 0; i < scols; i++) {
       if (Math.abs(this.s[i]) <= e) {
         Ls[i][i] = 0;
       } else {
@@ -455,12 +443,11 @@ export default class SingularValueDecomposition {
     var vrows = V.rows;
     var urows = U.length;
     var VLU = Matrix.zeros(vrows, urows);
-    var j, k, sum;
 
-    for (i = 0; i < vrows; i++) {
-      for (j = 0; j < urows; j++) {
-        sum = 0;
-        for (k = 0; k < scols; k++) {
+    for (let i = 0; i < vrows; i++) {
+      for (let j = 0; j < urows; j++) {
+        let sum = 0;
+        for (let k = 0; k < scols; k++) {
           sum += VL[i][k] * U[j][k];
         }
         VLU[i][j] = sum;
@@ -491,10 +478,9 @@ export default class SingularValueDecomposition {
     var vrows = V.length;
     var vcols = V[0].length;
     var X = new Matrix(vrows, this.s.length);
-    var i, j;
 
-    for (i = 0; i < vrows; i++) {
-      for (j = 0; j < vcols; j++) {
+    for (let i = 0; i < vrows; i++) {
+      for (let j = 0; j < vcols; j++) {
         if (Math.abs(this.s[j]) > e) {
           X[i][j] = V[i][j] / this.s[j];
         } else {
@@ -508,12 +494,11 @@ export default class SingularValueDecomposition {
     var urows = U.length;
     var ucols = U[0].length;
     var Y = new Matrix(vrows, urows);
-    var k, sum;
 
-    for (i = 0; i < vrows; i++) {
-      for (j = 0; j < urows; j++) {
-        sum = 0;
-        for (k = 0; k < ucols; k++) {
+    for (let i = 0; i < vrows; i++) {
+      for (let j = 0; j < urows; j++) {
+        let sum = 0;
+        for (let k = 0; k < ucols; k++) {
           sum += X[i][k] * U[j][k];
         }
         Y[i][j] = sum;
