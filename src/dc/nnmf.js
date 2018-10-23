1dcab6f65af6cb75abcb49de04ea8abe5f918b45
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-10-23 18:38:48 +0200

    Fix for nnmf

--- src/dc/nnmf.js
+++ src/dc/nnmf.js
@@ -1,4 +1,4 @@
-import { Matrix } from '../index';
+import { Matrix, WrapperMatrix2D } from '../index';
 
 /**
  * @class Non-negativeMatrixFactorization
@@ -7,77 +7,83 @@ import { Matrix } from '../index';
  * @param {number} it
  */
 export default class NNMF {
+  constructor(A, r) {
+    A = WrapperMatrix2D.checkMatrix(A);
+    var m = this.A.rows;
+    var n = this.A.columns;
+
+    let X = Matrix.rand(m, r);
+    let Y = Matrix.rand(r, n);
+  }
   /**
  * Do the NNMF of a matrix A into two matrix X and Y
- * @param {Matrix} A
+ * @param {Matrix} this.A
  * @param {number} r
  * @param {number} it
  * @return {Array} [X,Y]
  */
-  nnmf(A, r, it) {
-    var m = A.rows;
-    var n = A.columns;
+  nnmf(A, it) {
+    let A2 = this.X.mmul(this.Y);
 
-    let X = Matrix.rand(m, r);
-    let Y = Matrix.rand(r, n);
-    let A2 = X.mmul(Y);
+    let numX = Matrix.empty(this.m, this.r);
+    let denumX = Matrix.empty(this.m, this.r);
 
-    let numX = Matrix.empty(m, r);
-    let denumX = Matrix.empty(m, r);
+    let numY = Matrix.empty(this.r, this.n);
+    let denumY = Matrix.empty(this.r, this.n);
 
-    let numY = Matrix.empty(r, n);
-    let denumY = Matrix.empty(r, n);
-
-    let temp1 = Matrix.empty(m, n);
-    let temp2 = Matrix.empty(n, m);
+    let temp1 = Matrix.empty(this.m, this.n);
+    let temp2 = Matrix.empty(this.n, this.m);
 
     for (let a = 0; a < it; a++) {
-      for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-          temp1.set(i, j, Math.pow(A2.get(i, j), -2) * A.get(i, j));
+      for (let i = 0; i < this.m; i++) {
+        for (let j = 0; j < this.n; j++) {
+          temp1.set(i, j, Math.pow(A2.get(i, j), -2) * this.A.get(i, j));
           temp2.set(i, j, Math.pow(A2.get(i, j), -1));
         }
       }
-      numX = temp1.mmul(Y.transpose());
-      denumX = temp2.mmul(Y.transpose());
-      for (let i = 0; i < m; i++) {
-        for (let j = 0; j < r; j++) {
+      numX = temp1.mmul(this.Y.transpose());
+      denumX = temp2.mmul(this.Y.transpose());
+      for (let i = 0; i < this.m; i++) {
+        for (let j = 0; j < this.r; j++) {
           numX.set(i, j, numX.get(i, j) + Number.EPSILON);
           denumX.set(i, j, denumX.get(i, j) + Number.EPSILON);
         }
       }
-      for (let i = 0; i < m; i++) {
-        for (let j = 0; j < r; j++) {
-          X.set(i, j, X.get(i, j) * numX.get(i, j) / denumX.get(i, j));
+      for (let i = 0; i < this.m; i++) {
+        for (let j = 0; j < this.r; j++) {
+          this.X.set(i, j, this.X.get(i, j) * numX.get(i, j) / denumX.get(i, j));
         }
       }
-      A2 = X.mmul(Y);
-      for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
+      A2 = this.X.mmul(this.Y);
+      for (let i = 0; i < this.m; i++) {
+        for (let j = 0; j < this.n; j++) {
           A2.set(i, j, A2.get(i, j) + Number.EPSILON);
         }
       }
 
-      for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
+      for (let i = 0; i < this.m; i++) {
+        for (let j = 0; j < this.n; j++) {
           temp1.set(i, j, Math.pow(A2.get(i, j), -2) * A.get(i, j));
           temp2.set(i, j, Math.pow(A2.get(i, j), -1));
         }
       }
-      numY = X.transpose().mmul(temp1);
-      denumY = X.transpose().mmul(temp2);
-      for (let i = 0; i < r; i++) {
-        for (let j = 0; j < n; j++) {
+      numY = this.X.transpose().mmul(temp1);
+      denumY = this.X.transpose().mmul(temp2);
+      for (let i = 0; i < this.r; i++) {
+        for (let j = 0; j < this.n; j++) {
           numY.set(i, j, numY.get(i, j) + Number.EPSILON);
           denumY.set(i, j, denumY.get(i, j) + Number.EPSILON);
         }
       }
-      for (let i = 0; i < r; i++) {
-        for (let j = 0; j < n; j++) {
-          Y.set(i, j, Y.get(i, j) * numY.get(i, j) / denumY.get(i, j));
+      for (let i = 0; i < this.r; i++) {
+        for (let j = 0; j < this.n; j++) {
+          this.Y.set(i, j, this.Y.get(i, j) * numY.get(i, j) / denumY.get(i, j));
         }
       }
     }
-    return [X, Y];
+    return [this.X, this.Y];
+  }
+  error() {
+
   }
 }
