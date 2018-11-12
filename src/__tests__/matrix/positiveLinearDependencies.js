6988d6190343fae1839c7a7e3301d6fae55c74ba
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-11-12 11:16:42 +0100

    Add a test for positiveLinearCombination

---
+++ src/__tests__/matrix/positiveLinearDependencies.js
@@ -0,0 +1,26 @@
+import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
+
+import { Matrix, positiveLinearDependencies } from '../..';
+
+expect.extend({ toBeDeepCloseTo });
+
+describe('Non-negative Matrix Factorization', () => {
+  it('Base I', () => {
+    let base = new Matrix([
+      [0, 20, 100, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 30, 100, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 100, 5, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 100, 15, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 10, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 10],
+    ]);
+    let vector = new Matrix([[0, 20, 100, 20, 0, 0, 0, 0, 0, 5, 100, 5, 0, 0, 0, 20, 200, 20]]);
+    let solutions = Matrix.empty(1, base.columns);
+    let expected = new Matrix([1, 0, 1, 0, 0, 2]);
+
+    solutions = positiveLinearDependencies.positiveLinearDependencies(base, vector);
+
+    expect(solutions).toEqual(expected);
+  });
+});
+
