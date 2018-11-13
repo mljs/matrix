72060b425ee299e5b69f9a6529b3cce0f7e93ec0
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-11-13 10:07:20 +0100

    Add test for decimal

--- src/__tests__/matrix/positiveLinearCombination.js
+++ src/__tests__/matrix/positiveLinearCombination.js
@@ -14,7 +14,8 @@ describe('Positive linear combination', () => {
       [0, 0, 0, 0, 0, 0, 0, 10, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 10],
     ]);
-    let vector = new Matrix([[0, 20, 100, 20, 0, 0, 0, 0, 0, 5, 100, 5, 0, 0, 0, 20, 200, 20]]);
+    let vector = new Matrix(
+      [[0, 20, 100, 20, 0, 0, 0, 0, 0, 5, 100, 5, 0, 0, 0, 20, 200, 20]]);
     let solutions = Matrix.zeros(1, base.columns);
     let expected = new Matrix([[1, 0, 1, 0, 0, 2]]);
 
@@ -22,5 +23,23 @@ describe('Positive linear combination', () => {
 
     expect(solutions).toEqual(expected);
   });
-});
+  it('Decimal Base I', () => {
+    let base = new Matrix([
+      [0, 20, 100, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 30, 100, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 100, 5, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 100, 15, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 10, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 10],
+    ]);
+    let vector = new Matrix(
+      [[0, 20, 100, 20, 0, 0, 0, 0, 0, 5, 100, 5, 0, 0, 0, 5, 50, 5]]);
+    let solutions = Matrix.zeros(1, base.columns);
+    let expected = new Matrix([[1, 0, 1, 0, 0, 0.5]]);
 
+    solutions = positiveLinearCombination(base, vector, { lowestDecimal: 0.5 });
+
+    console.table(solutions);
+    expect(solutions).toEqual(expected);
+  });
+});
