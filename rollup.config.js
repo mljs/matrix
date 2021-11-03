3cbc4c295d896a895c81ab88395ee08518fa3814
josoriom <javier.osorio@correounivalle.edu.co>
2021-11-03 12:49:25 -0500

    chore: update typedoc.yml

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonJS from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default [
