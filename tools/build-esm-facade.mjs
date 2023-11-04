542dd466e16191fa617374a6dc176a7afa408f9b
Michaël Zasso <targos@protonmail.com>
2023-11-04 10:26:47 +0100

    fix: make default import work in TS esModuleInterop
    Closes: https://github.com/mljs/matrix/issues/175

--- tools/build-esm-facade.mjs
+++ tools/build-esm-facade.mjs
@@ -6,14 +6,14 @@ const exportNames = Object.keys(matrixBuild);
 
 const exports = exportNames.map((name) => {
   if (name === 'default') {
-    return `export default matrix.default;`;
+    return `export default matrix.default.Matrix ? matrix.default.Matrix : matrix.Matrix;`;
   }
   return `export const ${name} = matrix.${name};`;
 });
 
 await writeFile(
   new URL('../matrix.mjs', import.meta.url),
-  `import matrix from './matrix.js';
+  `import * as matrix from './matrix.js';
 
 ${exports.join('\n')}
 `,
